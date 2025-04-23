67d351b394baa883ac9ea2aeef60d797937415a6
apsu96 <apsu0161@gmail.com>
2025-04-23 15:22:13 -0500

    add netlify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import { CargoForm } from "~/app/_components/Cargo";
-import { HydrateClient, api } from "~/trpc/server";
 import CargoTable from "./_components/Table";
+import { CargoForm } from "./_components/Cargo";
+import { HydrateClient } from "~/trpc/server";
 import { DestinationBarChart } from "./_components/DestinationBarChart";
 import { StatusBarChart } from "./_components/StatusBarChart";
 
 export default async function Home() {
-  void api.cargo.getAll.prefetch();
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#8e568d] to-[#15162c] text-white">
